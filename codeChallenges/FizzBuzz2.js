0394df0130a8ed4a8f1e7a27782b5fab928c9701
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-01-11 20:16:42 -0800

    Completed 3 coding challenges

---
+++ codeChallenges/FizzBuzz2.js
@@ -0,0 +1,7 @@
+// Write a JavaScript program that prints out a String or Number: The printed value will depend on the value of a Number. If the Number is a multiple of 3, print "Fizz". If the Number is a multiple of 5, print "Buzz". If the Number is a multiple of both 3 and 5, print "FizzBuzz". If the Number is not a multiple of either, print the Number itself.
+
+let i = 21;
+let output = "";
+if (!(i%3)) output += "Fizz";
+if (!(i%5)) output += "Buzz";
+console.log(output?output:i);

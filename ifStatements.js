7d5ee6e7b4fee7f3cda88ffb9f31df8c67c3b9cb
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-01-11 19:10:32 -0800

    Completed ifStatements.js

--- ifStatements.js
+++ ifStatements.js
@@ -10,9 +10,11 @@
 
 var numQuarters = 0;
 
-console.log("I have enough money for a gumball");
-console.log("I don't have enough money for a gumball");
-
+if(numQuarters >= 2) {
+  console.log("I have enough money for a gumball");
+} else {
+  console.log("I don't have enough money for a gumball");
+}
 
 // CHALLENGE 2
 // Using the variables defined below, write code that will tell you if you have the ingredients to make a pizza. 
@@ -29,3 +31,8 @@ console.log("I don't have enough money for a gumball");
 var cupsOfFlour = 1;
 var hasSauce = true;
 
+if (cupsOfFlour >= 2 && hasSauce) {
+  console.log(`I can make pizza.`);
+} else {
+  console.log(`I cannot make pizza.`);
+}
